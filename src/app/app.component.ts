2d33024a6130d519b9c9a6d922cf0bde7621ea47
Christa Stephens <christastephens@quickenloans.com>
2020-02-27 16:09:54 -0500

    added ng oninit with ng onchanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent implements OnChanges, OnInit {
+ngOnInit(): void{
+  alert("2. on init is called");
+}
+ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
 
+}
 
   name: string = 'Christa\'s App';
   title = 'ngcanvas';
 
+  constructor(){
+    alert("1. on changes called");
+  }
 }
