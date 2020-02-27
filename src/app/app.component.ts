3fe9e17ee0239194fe59ca2d568c62784b0878e3
Christa Stephens <christastephens@quickenloans.com>
2020-02-27 15:44:32 -0500

    onchanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
+  ngOnChanges(changed: import("@angular/core").SimpleChanges): void {
+
+  }
   name: string = 'Christa\'s App';
+  title = 'ngcanvas';
+  constructor() {
+    alert("1. on changes called");
+  }
 }
