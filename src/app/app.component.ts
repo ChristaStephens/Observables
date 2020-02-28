7fe39ef6246f68941b07feda2d4cc29b439f354e
Christa Stephens <christastephens@quickenloans.com>
2020-02-28 09:26:52 -0500

    comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component, OnChanges, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnChanges, OnInit {
+  //OnInit is only called once, to initialize the component
+  //it sets and displays th components input properties
+  //It's the most important lifecycle hook in Angular,
+  //as it signals the activation of the created component.
 ngOnInit(): void{
   alert("2. on init is called");
 }
