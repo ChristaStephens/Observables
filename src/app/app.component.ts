7fce3e447f1bbf1ab789aee56ceb8b33b07bdced
Christa Stephens <christastephens@quickenloans.com>
2020-02-28 10:47:59 -0500

    reset ngdocheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,13 @@ import { Component, OnChanges } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges {
-  //called right after your class gets initialized
-  //and the component is created, this lifecyle hook is called.
-  //this is basically called after the constructor is called
-  //and any other time there is a property change w/in a component
-  ngOnChanges(changed: import("@angular/core").SimpleChanges): void {
+export class AppComponent {
+  ngOnChanges() {
 
   }
   name: string = 'Christa\'s App';
   title = 'ngcanvas';
   constructor() {
-    alert("1. on changes called");
+
   }
 }
